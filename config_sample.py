9d06849159ddb3e69cec2aabcfd49683902edfb8
ajayss4628-hub <ajayss4628@gmail.com>
2026-06-21 22:26:49 +0530

    Update config_sample.py
    Signed-off-by: ajayss4628-hub <ajayss4628@gmail.com>

--- config_sample.py
+++ config_sample.py
@@ -12,7 +12,7 @@ TG_PROXY = (
 )  # {"scheme": ”socks5”, "hostname": ””, "port": 1234, "username": ”user”, "password": ”pass”}
 USER_SESSION_STRING = ""
 CMD_SUFFIX = ""
-AUTHORIZED_CHATS = ""
+AUTHORIZED_CHATS = "-5518685191"
 SUDO_USERS = ""
 STATUS_LIMIT = 10
 DEFAULT_UPLOAD = "rc"
